2d4b67c5a2a7da92150761a9538b5b5f5bbacffd
hullen <hullenn@gmail.com>
2020-08-17 13:21:07 -0300

    refactor: code style formated

--- DragNDrop/DropArea.jsx
+++ DragNDrop/DropArea.jsx
@@ -152,7 +152,11 @@ export default function DropArea() {
         const condPrev = regras[index - 2] || {};
         const condNext = regras[index + 2] || {};
 
-        if (regraType === ItemTypes.CONDITIONAL && condPrev.cond && condPrev.cond !== condDropped) {
+        if (
+          regraType === ItemTypes.CONDITIONAL &&
+          condPrev.cond &&
+          condPrev.cond !== condDropped
+        ) {
           console.log('regra anterior diferente');
           notification.error({
             message: 'Não é possível adicionar operador',
@@ -160,7 +164,11 @@ export default function DropArea() {
               condPrev.cond
             }).`,
           });
-        } else if (regraType === ItemTypes.CONDITIONAL &&condNext.cond && condNext.cond !== condDropped) {
+        } else if (
+          regraType === ItemTypes.CONDITIONAL &&
+          condNext.cond &&
+          condNext.cond !== condDropped
+        ) {
           console.log('regra proxima diferente');
           notification.error({
             message: 'Não é possível adicionar operador',
